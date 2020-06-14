cc7141efbc1d0bcfe50f6888f7b6f5b06bd2d41e
ibrahimtaj <taj_it@yahoo.com>
2020-06-14 15:44:03 +0530

    Updates

---
+++ precache-manifest.3ed9144d5485aa2dcc86518235ff6646.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62451196bad10270e27f688b417dfecb",
+    "url": "/my-admin/index.html"
+  },
+  {
+    "revision": "4d5825e220eafd177d4f",
+    "url": "/my-admin/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "54aeae82db480ea696c7",
+    "url": "/my-admin/static/css/main.5acb0982.chunk.css"
+  },
+  {
+    "revision": "4d5825e220eafd177d4f",
+    "url": "/my-admin/static/js/2.99ad5937.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-admin/static/js/2.99ad5937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54aeae82db480ea696c7",
+    "url": "/my-admin/static/js/main.a500d231.chunk.js"
+  },
+  {
+    "revision": "9ad7441ca26e0da5119f",
+    "url": "/my-admin/static/js/runtime-main.60d66e70.js"
+  }
+]);
